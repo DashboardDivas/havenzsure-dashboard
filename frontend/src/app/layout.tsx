39654eba32e0d0bd5e235ded43369dad517a6c45
Himanshi <Himanshi.punj@edu.sait.ca>
2025-11-25 12:34:54 -0700

    Updated Login Page

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,19 +2,13 @@
 
 import React from "react";
 import CustomThemeProvider from "@/context/ThemeContext";
-import Navbar from "@/components/layout/Navbar";
-import Sidebar from "@/components/layout/Sidebar";
-
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const [sidebarOpen, setSidebarOpen] = React.useState(false);
 
   return (
     <html lang="en">
       <body>
         <CustomThemeProvider>
-          <Navbar onMenuClick={() => setSidebarOpen(true)} />
-          <Sidebar open={sidebarOpen} onClose={() => setSidebarOpen(false)} />
           {children}
         </CustomThemeProvider>
       </body>
